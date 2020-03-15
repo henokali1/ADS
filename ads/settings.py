7f9a1b9049a9fa80190d38211199d97070e491b9
Ubuntu <ubuntu@ip-172-31-32-200.ec2.internal>
2020-03-15 05:04:44 +0000

    AWS dip

--- ads/settings.py
+++ ads/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6_(1ss%*6ks2)*2%&)3(goc!=j)o@p_=i009qv1d9*#-ylb_f#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '54.162.193.161']
 
 
 # Application definition
